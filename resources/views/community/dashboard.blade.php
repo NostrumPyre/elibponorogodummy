b28fec78e07efcdcb895b1c8bdc63a3b9f0e6ba0
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 01:57:40 +0800

    commit

---
+++ resources/views/community/dashboard.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.master')
+@section('content')
+<div class="container-fluid main-container dashboard">
+    <div id="wrapper" class="">
+        <div id="sidebar-wrapper">
+
+            <ul class="sidebar-nav">
+                <li class="sidebar-section-title">
+                    <p class="text-uppercase">Visibility</p>
+                <li>
+                    <a class="visibility-filter all-bookmarks selected" href="#">
+                        <i class="fa fa-file-text-o"></i>
+                        <span class="filter-name">All</span>
+
+                    </a>
+                </li>
+                <li>
+                    <a class="visibility-filter public-bookmarks" href="#">
+                        <i class="fa fa-file-text-o"></i>
+                        <span class="filter-name">Public</span>
+
+                    </a>
+                </li>
+                <li>
+                    <a class="visibility-filter my-bookmarks" href="#">
+                        <i class="fa fa-file-text-o"></i>
+                        <span class="filter-name">Private</span>
+
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+</div>
+@include('layouts.new-bookmark')
+@include('layouts.update-bookmark')
+@endsection
