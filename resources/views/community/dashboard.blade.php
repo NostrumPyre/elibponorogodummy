ac2dc0d3342de800c9d433acf219daad7caa81f9
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 14:52:17 +0800

    commit

--- resources/views/community/dashboard.blade.php
+++ resources/views/community/dashboard.blade.php
@@ -27,27 +27,28 @@
           @endforeach
           <br>
           @foreach($journals as $key => $data)
-          <div class="col col-sm-4">
-            <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top" alt="...">
-          </div>
-          <div class="col text-start m-4 " style="color: #212529">
-            <div class="row">
-              <div class="col">
-                <h3 class="card-title">{{$data->title}}</h3>
-              </div>
-              <div class="col me-0 text-end">
-                <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
-              </div>
+          <div class="row">
+            <div class="col col-sm-4">
+              <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top" alt="...">
             </div>
-            <p class="card-text mt-3">{{$data->description}}</p>
-            <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
+            <div class="col text-start m-4 " style="color: #212529">
+              <div class="row">
+                <div class="col">
+                  <h3 class="card-title">{{$data->title}}</h3>
+                </div>
+                <div class="col me-0 text-end">
+                  <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
+                </div>
+              </div>
+              <p class="card-text mt-3">{{$data->description}}</p>
+              <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
+            </div>
+            @endforeach
+
           </div>
-          @endforeach
 
         </div>
-
       </div>
     </div>
-  </div>
 </header>
 @endsection
