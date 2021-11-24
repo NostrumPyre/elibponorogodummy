214528d1ee83cdcecfae228bd40ae204c9d3ca4f
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 14:44:21 +0800

    commit

--- resources/views/community/dashboard.blade.php
+++ resources/views/community/dashboard.blade.php
@@ -7,7 +7,7 @@
       <div class="text-center text-white d-flex justify-content-center">
 
         <div class="row p-5 bg-light rounded" style="width: 960px !important; margin-top: 30px; margin-bottom:30px;">
-          <h1>Collection</h1>
+          <h1 style="color: #212529">Collection</h1>
           @foreach($books as $key => $data)
           <div class="col col-sm-4">
             <img src="https://drive.google.com/uc?export=view&id=14r5VEmUBjaVOYQgJ8sZrRXRTLK15YJvv" class="card-img-top" alt="...">
