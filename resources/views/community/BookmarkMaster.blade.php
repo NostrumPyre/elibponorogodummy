7a0cbbf8107810cca214b09bbc0b969b5fa5abac
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 13:11:50 +0800

    commit

--- resources/views/community/BookmarkMaster.blade.php
+++ resources/views/community/BookmarkMaster.blade.php
@@ -12,7 +12,7 @@
     <link href="{{ asset('assets/vendor/tag-it/css/jquery.tagit.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/fontawesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/animate-css/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('assets/style.css') }}" rel="stylesheet">
+    <link href="{{URL::asset('css/styles.css')}}" rel="stylesheet" />
     <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/jquery-ui/jquery-ui.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
@@ -20,7 +20,7 @@
 
 </head>
 
-<body {{ Request::is('dashboard') ? 'class="dashboard"' : ""}}>
+<body>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
