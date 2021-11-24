5db9731ef478603866d472bf247576c8ea1d4996
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 15:12:25 +0800

    commit

--- resources/views/community/dashboard.blade.php
+++ resources/views/community/dashboard.blade.php
@@ -30,7 +30,7 @@
 
         @foreach($journals as $key => $data)
         <div class="text-center row p-5 bg-light rounded" style="width: 960px !important; margin-top: 30px; margin-bottom:30px;">
-          <h1 style="color: #212529">Book Collection</h1>
+          <h1 style="color: #212529">Journal Collection</h1>
           <div class="col col-sm-4 ">
             <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top " alt="...">
           </div>
