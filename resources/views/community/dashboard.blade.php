7fea845050e8b8bd6bd7503db5a9f47ecc2faa9b
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 14:57:15 +0800

    commit

--- resources/views/community/dashboard.blade.php
+++ resources/views/community/dashboard.blade.php
@@ -24,32 +24,34 @@
             <p class="card-text mt-3">{{$data->description}}</p>
             <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
           </div>
-          @endforeach
-
-
-          @foreach($journals as $key => $data)
-          <div class="row mt-5">
-            <div class="col col-sm-4 ">
-              <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top " alt="...">
-            </div>
-            <div class="col text-start m-4 " style="color: #212529">
-              <div class="row">
-                <div class="col">
-                  <h3 class="card-title">{{$data->title}}</h3>
-                </div>
-                <div class="col me-0 text-end">
-                  <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
-                </div>
-              </div>
-              <p class="card-text mt-3">{{$data->description}}</p>
-              <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
-            </div>
-            @endforeach
-
-          </div>
-
         </div>
+        @endforeach
+
+
+        @foreach($journals as $key => $data)
+        <div class="row p-5 bg-light rounded" style="width: 960px !important; margin-top: 30px; margin-bottom:30px;">
+          <div class="col col-sm-4 ">
+            <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top " alt="...">
+          </div>
+          <div class="col text-start m-4 " style="color: #212529">
+            <div class="row">
+              <div class="col">
+                <h3 class="card-title">{{$data->title}}</h3>
+              </div>
+              <div class="col me-0 text-end">
+                <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
+              </div>
+            </div>
+            <p class="card-text mt-3">{{$data->description}}</p>
+            <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
+          </div>
+        </div>
+        @endforeach
+
+
+
       </div>
     </div>
+  </div>
 </header>
 @endsection
