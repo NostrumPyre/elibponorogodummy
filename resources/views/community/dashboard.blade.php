24cc6353783630a6c2a3cd777eefd6bf3bd57fd6
NostrumPyre <watchdogs13.xv@gmail.com>
2021-11-24 14:48:49 +0800

    commit

--- resources/views/community/dashboard.blade.php
+++ resources/views/community/dashboard.blade.php
@@ -8,43 +8,42 @@
 
         <div class="row p-5 bg-light rounded" style="width: 960px !important; margin-top: 30px; margin-bottom:30px;">
           <h1 style="color: #212529">Collection</h1>
-          @foreach($books as $key => $data)
+          @foreach($books as $key => $databook, $journals as $key => $datajournal)
           <div class="col col-sm-4">
             <img src="https://drive.google.com/uc?export=view&id=14r5VEmUBjaVOYQgJ8sZrRXRTLK15YJvv" class="card-img-top" alt="...">
           </div>
           <div class="col text-start m-4 " style="color: #212529">
             <div class="row">
               <div class="col">
-                <h3 class="card-title">{{$data->title}}</h3>
+                <h3 class="card-title">{{$databook->title}}</h3>
               </div>
               <div class="col me-0 text-end">
                 <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
               </div>
             </div>
-            <p class="card-text mt-3">{{$data->description}}</p>
+            <p class="card-text mt-3">{{$databook->description}}</p>
             <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
           </div>
-          @endforeach
-
-          @foreach($journals as $key => $data)
           <div class="col col-sm-4">
             <img src="https://drive.google.com/uc?export=view&id=1kaOdo0gexiT6Xj0cyLq4rIE0fpBdD2KR" class="card-img-top" alt="...">
           </div>
-          <div class="col text-start m-4 " style="color: #212529">
-            <div class="row">
-              <div class="col">
-                <h3 class="card-title">{{$data->title}}</h3>
-              </div>
-              <div class="col me-0 text-end">
-                <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
-              </div>
+          div class="col text-start m-4 " style="color: #212529">
+          <div class="row">
+            <div class="col">
+              <h3 class="card-title">{{$datajournal->title}}</h3>
             </div>
-            <p class="card-text mt-3">{{$data->description}}</p>
-            <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
+            <div class="col me-0 text-end">
+              <i class="bi bi-bookmark-fill" style="font-size: 25px;" name="bookmark-fill"></i>
+            </div>
           </div>
-          @endforeach
-
+          <p class="card-text mt-3">{{$datajournal->description}}</p>
+          <a class="btn btn-primary mt-5" href="{{route('download', ["File_Upload" => "$data->filepath"])}}" style="background-color: #008000; border: #008000">Download</a>
         </div>
+        @endforeach
+        <!-- 
+        @foreach($journals as $key => $data)
+
+        < @endforeach </div> -->
 
       </div>
     </div>
